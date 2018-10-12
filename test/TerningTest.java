bd75336984b54846eea140a65fe16b42868dfdb8
Theodor Lysberg Michaelsen <s174882@student.dtu.dk>
2018-10-12 11:00:34 +0200

    TerningTest af fordeling af slag

--- test/TerningTest.java
+++ test/TerningTest.java
@@ -33,4 +33,52 @@ class TerningTest {
         assertEquals(terning.getResultat(), slag);
         assertTrue(terning.getResultat() >= 1 && terning.getResultat() <= 6);
     }
+
+    /**
+     * Tester at alle seks værdier forekommer lige hyppigit indenfor en passende fejlmargin,
+     * som i dette tilfælde er sat til +-400, ved 60000 kast.
+     * @result Resultatet lever op til kravene
+     */
+    @Test
+    void fordelingAfSlag(){
+        int et = 0, to = 0, tre = 0, fire = 0, fem = 0, seks = 0;
+
+        for (int i = 0; i < 60000; i++) {
+            int nyTerningSlag = terning.nyTerningSlag();
+
+            if(nyTerningSlag == 1){
+                et = et + 1;
+            }
+            else if(nyTerningSlag == 2){
+                to = to + 1;
+            }
+            else if(nyTerningSlag == 3){
+                tre = tre + 1;
+            }
+            else if(nyTerningSlag == 4){
+                fire = fire + 1;
+            }
+            else if(nyTerningSlag == 5){
+                fem = fem + 1;
+            }
+            else if(nyTerningSlag == 6){
+                seks = seks + 1;
+            }
+
+            if(i == 59999){
+                System.out.println("1 " + et + "\n" + "2 " + to + "\n" + "3 " + tre + "\n" +
+                        "4 " + fire + "\n" + "5 " + fem + "\n" + "6 " + et + "\n");
+            }
+        }
+
+        assertTrue(et >= 10000 - 400 && et <= 10000 + 400);
+        assertTrue(to >= 10000 - 400 && et <= 10000 + 400);
+        assertTrue(tre >= 10000 - 400 && et <= 10000 + 400);
+        assertTrue(fire >= 10000 - 400 && et <= 10000 + 400);
+        assertTrue(fem >= 10000 - 400 && et <= 10000 + 400);
+        assertTrue(seks >= 10000 - 400 && et <= 10000 + 400);
+    }
+
+
+
 }
