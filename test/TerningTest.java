2d0528e3f24f9ffe17c7e36801595e15523b68a1
Theodor Lysberg Michaelsen <s174882@student.dtu.dk>
2018-10-12 13:31:45 +0200

    TerningTest af slag med to terningers normalfordeling

--- test/TerningTest.java
+++ test/TerningTest.java
@@ -1,6 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 class TerningTest {
     private Terning terning;
 
@@ -37,7 +40,6 @@ class TerningTest {
     /**
      * Tester at alle seks værdier forekommer lige hyppigit indenfor en passende fejlmargin,
      * som i dette tilfælde er sat til +-400, ved 60000 kast.
-     * @result Resultatet lever op til kravene
      */
     @Test
     void fordelingAfSlag(){
@@ -78,6 +80,44 @@ class TerningTest {
         assertTrue(fem >= 10000 - 400 && et <= 10000 + 400);
         assertTrue(seks >= 10000 - 400 && et <= 10000 + 400);
     }
+
+    /**
+     * Tester at et slag med to terninger, giver en sum der er normaltfordelt.
+     */
+    @Test
+    void testAfRaflebaeger(){
+        int[] slag = new int[11];
+
+        Raflebaeger raflebaeger = new Raflebaeger();
+
+        for (int i = 0; i <= 1000; i++) {
+
+            int nyTerningSlag = raflebaeger.slaaTerninger()[2];
+            slag[nyTerningSlag - 2]++;
+        }
+
+        System.out.println(Arrays.toString(slag));
+        for(int i = 0; i < slag.length - 1; i++){
+            System.out.println((i + 1) + " : " + slag[i] + " ");
+        }
+
+        assertTrue(slag[0] >= ((1000*1/36)-40) && slag[0] <= ((1000*1/36)+40));
+        assertTrue(slag[1] >= ((1000*2/36)-40) && slag[1] <= ((1000*2/36)+40));
+        assertTrue(slag[2] >= ((1000*3/36)-40) && slag[2] <= ((1000*3/36)+40));
+        assertTrue(slag[3] >= ((1000*4/36)-40) && slag[3] <= ((1000*4/36)+40));
+        assertTrue(slag[4] >= ((1000*5/36)-40) && slag[4] <= ((1000*5/36)+40));
+        assertTrue(slag[5] >= ((1000*6/36)-40) && slag[5] <= ((1000*6/36)+40));
+        assertTrue(slag[6] >= ((1000*5/36)-40) && slag[6] <= ((1000*5/36)+40));
+        assertTrue(slag[7] >= ((1000*4/36)-40) && slag[7] <= ((1000*4/36)+40));
+        assertTrue(slag[8] >= ((1000*3/36)-40) && slag[8] <= ((1000*3/36)+40));
+        assertTrue(slag[9] >= ((1000*2/36)-40) && slag[9] <= ((1000*2/36)+40));
+        assertTrue(slag[10] >= ((1000*1/36)-40) && slag[10] <= ((1000*1/36)+40));
+
+
+
+
+    }
+
 
 
 
